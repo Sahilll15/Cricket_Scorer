aa834aa4aab3957ba5659a0db386f99aedaad362
SAHIL SANJAY CHALKE <SahilCHALKE1011@gmail.com>
2023-02-26 04:26:36 +0530

    new commit

--- scoring/urls.py
+++ scoring/urls.py
@@ -2,9 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('',views.home,name="home"),
-    path('login/',views.login,name="login"),
-    path('score/',views.score,name="score")
+    # path('',views.home,name="home"),
+
+    path('login/',views.login_view,name="login"),
+    path('',views.signup,name="signup"),
+    path('score/',views.score,name="score"),
+    path('logout/',views.logout_view,name="logout"),
+    path('makematch/',views.make_match,name="make_match"),
+    path('matchtoss/',views.match_toss,name="matchtoss"),
+    path('scoring/',views.scoring,name="scoring"),
+
+
+    
 
 
 
--- scoring/views.py
+++ scoring/views.py
@@ -1,14 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login,logout
+from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
+def signup(request):
+    
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        uemail=request.POST.get('email')
+        upassword=request.POST.get('password')
+        confirm_password=request.POST.get('confirm_password')
+        
 
-def login(request):
-    return render(request,'login.html')
+        if upassword!=confirm_password:
+            return HttpResponse("The passwords in the two password fields do not match")
+        else:
+
+            my_user=User.objects.create_user(username=uname,email=uemail,password=upassword)
+            my_user.save()
+            # return HttpResponse("User has been Created succesfully!")
+            return redirect('login')
+            # print(uname,uemail,upassword,confirm_password)
+
+    
+    return render(request,'scoring/signup.html')
+
+
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('score')  # replace with your desired URL after successful login
+        else:
+            # return render(request, 'login.html', {'error_message': 'Invalid username or password'})
+            return HttpResponse("Invalid password or username")
+    return render(request, 'scoring/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def make_match(request):
+    return render(request,'scoring/make_match.html') 
+
+def match_toss(request):
+    return render(request,'scoring/matchtoss.html')
 
 def score(request):
-    return render(request,'score.html')
+    return render(request,'scoring/score.html')
+    # return HttpResponse("You are at the score page")
+
+def scoring(request):
+    return render(request,'scoring/Scoring.html')
+
 
 def home(request):
     return HttpResponse("You are at the home page")
