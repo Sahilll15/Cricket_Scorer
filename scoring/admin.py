a0c61c39c675139667ed150a8b4974f64c698adc
SAHIL SANJAY CHALKE <SahilCHALKE1011@gmail.com>
2023-03-11 02:41:17 +0530

    11/3/2023

--- scoring/admin.py
+++ scoring/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Tournament,Team,Match
 # Register your models here.
+
+admin.site.register(Tournament)
+admin.site.register(Team)
+admin.site.register(Match)
